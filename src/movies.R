6377f9e1710673c003c36e0fe7f9191c6294a205
David Smith <DSmith@seamsglobal.com>
2019-07-26 14:33:08 +0100

    Adding favourite movie

--- src/movies.R
+++ src/movies.R
@@ -54,6 +54,7 @@ movies %>%
 
 # --- Report favourites --------------------------------------------------------
 print("Tom's favourite movie is 'The Big Lebowski'")
+print("Dave's favourite movie is '1984'")
 
 # TODO Declare your own favourite with a print statement here...
 
